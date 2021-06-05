8c7f1c17d959ce51a2f44524b6a7b2d3fdafff29
Matias Duhalde <matias.duhalde@uc.cl>
2021-06-04 20:27:05 -0400

    style: remove blank line

--- src/models/property.js
+++ src/models/property.js
@@ -24,7 +24,6 @@ module.exports = (sequelize, DataTypes) => {
       userId: {
         type: DataTypes.BIGINT,
         allowNull: false,
-        
         validate: {
           isNumeric: true,
         },
