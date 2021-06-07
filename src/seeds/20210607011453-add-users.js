5f541b21ca5b04b325322847151dd6fd26bcb3d0
Matias Duhalde <matias.duhalde@uc.cl>
2021-06-06 21:27:08 -0400

    feat: add user seeds

---
+++ src/seeds/20210607011453-add-users.js
@@ -0,0 +1,26 @@
+const { generateRandomUsers } = require('./utils/users');
+
+module.exports = {
+  up: async (queryInterface) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert('Users', generateRandomUsers(100), {});
+  },
+
+  down: async (queryInterface) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Users', null, {});
+  },
+};
