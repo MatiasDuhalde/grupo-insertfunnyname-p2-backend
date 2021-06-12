38c20c619420e2d02b100c78a5f4ca8e3762af70
Matias Duhalde <matias.duhalde@uc.cl>
2021-06-12 01:08:36 -0400

    refactor: better routes with param validation

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,66 +1,79 @@
-const KoaRouter = require('koa-router');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const ApiError = require('./utils/apiError');
+const KoaRouter = require('koa-router');
 
-const { validateIntParam } = require('./utils/utils');
+const ApiError = require('./utils/apiError');
+const { validateIntParam, authJWT, requiredParams } = require('./utils/utils');
 
 const router = new KoaRouter();
-
-require('dotenv').config();
 
 const DEFAULT_AVATAR = `https://png.pngtree.com/png-vector/20191026/ourlarge/\
 pngtree-avatar-vector-icon-white-background-png-image_1870181.jpg${''}`;
 
 router.param('userId', validateIntParam);
 
-router.post('user.create', '/users', async (ctx) => {
-  const {
-    firstName,
-    lastName,
-    email,
-    password,
-  } = ctx.request.body;
-  try {
-    const user = await ctx.orm.User.create({
-      firstName,
+router.post(
+  'user.create',
+  '/users',
+  requiredParams({
+    firstName: 'string',
+    lastName: 'string',
+    email: 'string',
+    password: 'string',
+  }),
+  async (ctx) => {
+    const {
+      firstName, //
       lastName,
       email,
-      hashedPassword: password,
-      avatarLink: DEFAULT_AVATAR,
-    });
-    // TODO: Return JWT somewhere??
-    ctx.body = {
-      userId: user.id,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      email: user.email,
-    };
-  } catch (error) {
-    const errors = {};
-    if (error instanceof ctx.orm.Sequelize.ValidationError) {
-      error.errors.forEach((errorItem) => {
-        errors[errorItem.path] = errorItem.message;
+      password,
+    } = ctx.request.body;
+    try {
+      const user = await ctx.orm.User.create({
+        firstName: firstName.trim(),
+        lastName: lastName.trim(),
+        email: email.trim(),
+        hashedPassword: password,
+        avatarLink: DEFAULT_AVATAR,
       });
-    } else {
-      errors.password = error.message;
+      ctx.body = {
+        email: user.email,
+      };
+    } catch (error) {
+      const errors = {};
+      if (error instanceof ctx.orm.Sequelize.ValidationError) {
+        error.errors.forEach((errorItem) => {
+          errors[errorItem.path] = errorItem.message;
+        });
+      } else {
+        errors.password = error.message;
+      }
+      throw new ApiError(400, 'Could not create user', {
+        errors,
+      });
     }
-    throw new ApiError(400, 'Could not create user', {
-      errors,
-    });
-  }
-});
+  },
+);
 
-router.post('auth.create', '/', async (ctx) => {
-  const { email, password } = ctx.request.body;
-  const user = await ctx.orm.User.findOne({ where: { email } });
-  const authenticated = user && (await bcrypt.compare(password, user.hashedPassword));
-  if (user && authenticated) {
+router.post(
+  'auth.create',
+  '/auth',
+  requiredParams({
+    email: 'string',
+    password: 'string',
+  }),
+  async (ctx) => {
+    const { email, password } = ctx.request.body;
+    if (password.length < 6) {
+      throw new ApiError(422, 'Invalid password');
+    }
+    const user = await ctx.orm.User.findOne({ where: { email: email.trim() } });
+    if (!user || !(await bcrypt.compare(password, user.hashedPassword))) {
+      throw new ApiError(401, 'Incorrect email or password');
+    }
     const token = jwt.sign({ sub: user.id }, process.env.JWT_SECRET);
     ctx.body = { token };
-  } else {
-    ctx.throw(401, 'Correo y/o contraseña incorrectos');
-  }
-});
+  },
+);
 
 module.exports = router;
