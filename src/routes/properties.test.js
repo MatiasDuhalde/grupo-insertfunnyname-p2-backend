8e65fb065fa0792df07f76099ec36c26645979d6
Matias Duhalde <matias.duhalde@uc.cl>
2021-06-13 19:12:18 -0400

    feat: complete create property endpoint testing

--- src/routes/properties.test.js
+++ src/routes/properties.test.js
@@ -3,12 +3,12 @@ const app = require('../app');
 
 const request = supertest(app.callback());
 
-describe('Property Routes', () => {
+describe('Property routes', () => {
   const dummyUser = {
     firstName: 'Test',
     lastName: 'User',
     email: 'test@gmail.com',
-    hashedPassword: 'passwordIIC',
+    hashedPassword: 'testuser',
     avatarLink: 'https://cdn.fakercloud.com/avatars/tgerken_128.jpg',
   };
 
@@ -40,34 +40,49 @@ describe('Property Routes', () => {
       listingType: 'rent',
     };
 
-    const authorizedPostProperty = (body) => 
-    {const req = request 
-      .post('/properties')
-      .auth(auth.token, { type: 'bearer' })
-      .set('Content-type', 'application/json')
-      console.log(req)
-      req.send(body)
+    const authorizedPostProperty = (body) => {
+      const req = request
+        .post('/properties')
+        .auth(auth.token, { type: 'bearer' })
+        .set('Content-type', 'application/json');
+      return req.send(body);
     };
 
-    const unauthorizedPostProperty = (body) => request
-      .post('/properties')
-      .set('Content-type', 'application/json')
-      .send(body);
+    const unauthorizedPostProperty = (body) => {
+      const req = request.post('/properties').set('Content-type', 'application/json');
+      return req.send(body);
+    };
 
-    describe('Property data is valid', () => {
+    describe('when user is authorized', () => {
       let response;
 
       beforeAll(async () => {
         response = await authorizedPostProperty(dummyProperty);
       });
 
-      test('reponse with 201 (created) status code', () => {
+      test('responds with 201 status code', () => {
         expect(response.status).toBe(201);
       });
 
-      // test('responds with JSON body type', () => {
-      //   expect(response.type).toEqual('application/json');
-      // });
+      test('responds with JSON body type', () => {
+        expect(response.type).toEqual('application/json');
+      });
+
+      test('responds with property id', () => {
+        expect(response.body).toHaveProperty('id');
+      });
+    });
+
+    describe('when user is not authorized', () => {
+      let response;
+
+      beforeAll(async () => {
+        response = await unauthorizedPostProperty(dummyProperty);
+      });
+
+      test('responds with 401 status code', () => {
+        expect(response.status).toBe(401);
+      });
     });
   });
 });
