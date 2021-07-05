a5657f21898a9ac8d10f71ceac0d5d53e15ccbe8
Matias Duhalde <matias.duhalde@uc.cl>
2021-07-04 23:55:34 -0400

    test: disable mailer in test environment

--- src/mailers/index.js
+++ src/mailers/index.js
@@ -2,14 +2,21 @@ const nodemailer = require('nodemailer');
 const emailConfig = require('../config/email');
 
 module.exports = function mailers(app) {
-  const transport = nodemailer.createTransport(emailConfig.provider, emailConfig.defaults);
-  // eslint-disable-next-line no-param-reassign
-  app.context.sendMail = async function sendMail(emailName, options, templateContext) {
-    const html = await this.render(`emails/${emailName}`, {
-      ...templateContext,
-      layout: false,
-      writeResp: false,
-    });
-    return transport.sendMail({ ...options, html });
-  };
+  if (process.env.NODE_ENV !== 'test') {
+    const transport = nodemailer.createTransport(emailConfig.provider, emailConfig.defaults);
+    // eslint-disable-next-line no-param-reassign
+    app.context.sendMail = async function sendMail(emailName, options, templateContext) {
+      const html = await this.render(`emails/${emailName}`, {
+        ...templateContext,
+        layout: false,
+        writeResp: false,
+      });
+      return transport.sendMail({ ...options, html });
+    };
+  } else {
+    // eslint-disable-next-line no-param-reassign
+    app.context.sendMail = async function sendMail() {
+      return new Promise((resolve) => resolve());
+    };
+  }
 };
